83d6c5102070e5564a6310189b6a24bde0095f0a
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-02-14 22:02:30 +0100

    feat: Added Animated splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,27 @@
 import '../global.css';
 
 import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect, useState } from 'react';
+import AnimatedSplashScreen from '~/components/AnimatedSplashScreen';
 
-export default function Layout() {
-  return <Stack />;
+export default function Layout () {
+  const [appReady, setAppReady] = useState(false)
+  const [splashAnimationFinished, setSplashAnimationFinished] = useState(false)
+  const [fontsLoaded] = useFonts({
+    'SpaceMono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      setAppReady(true)
+    }
+  }, [fontsLoaded]);
+
+  if (!appReady || !splashAnimationFinished) {
+    return <AnimatedSplashScreen onAnimationFinish={(isCancelled) => {
+      if (!isCancelled) setSplashAnimationFinished(true)
+    }} />;
+  }
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
---
+++ components/AnimatedSplashScreen.tsx
@@ -0,0 +1,29 @@
+import { View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+interface AnimatedSplashScreenProps {
+	onAnimationFinish?: (isCancelled: boolean) => void;
+}
+
+const AnimatedSplashScreen = ({ onAnimationFinish = (isCancelled) => { } }: AnimatedSplashScreenProps) => {
+
+	return (
+		<View className='bg-white flex-1 items-center justify-center'>
+			<LottieView
+				autoPlay
+				onAnimationFinish={onAnimationFinish}
+				loop={false}
+				style={{
+					width: "80%",
+					minWidth: 300,
+					minHeight: 300,
+					backgroundColor: 'transparent',
+				}}
+				source={require('../assets/lotties/bellfast-lottie.json')}
+			/>
+		</View>
+	);
+}
+
+
+export default AnimatedSplashScreen;
